e9038b166d13a6495755d7e2d58acd8c9d39aeac
Joshua Oguma <joshua.oguma@outlook.com>
2023-05-21 15:38:14 +0300

    added some changes

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -9,35 +9,35 @@ export CUT_PREVIOUS_VERSION=$(echo ${PREVIOUS_VERSION} | cut -d. -f1-2)
 export CUT_LATEST_VERSION=$(echo ${LATEST_VERSION} | cut -d. -f1-2)
 export PREVIOUS_VERSIONS_DIR=previous_versions
 
-version_greater_equal() {
-    printf '%s\n%s\n' "$2" "$1" | sort --check=quiet --version-sort
-}
+# version_greater_equal() {
+#     printf '%s\n%s\n' "$2" "$1" | sort --check=quiet --version-sort
+# }
 
-save_versions_list() {
-    ls ${DEST}/${PREVIOUS_VERSIONS_DIR} --hide versions-list.json | awk ' BEGIN {ORS = ""; print "[";}' | sed "s^\^\\\\\"^g;s^\/\@\/\@^\",\"^g;s^\/\@^\"^g" > ${DEST}/${PREVIOUS_VERSIONS_DIR}/versions-list.json || echo "No previous versions"
-}
+# save_versions_list() {
+#     ls ${DEST}/${PREVIOUS_VERSIONS_DIR} --hide versions-list.json | awk ' BEGIN {ORS = ""; print "[";}' | sed "s^\^\\\\\"^g;s^\/\@\/\@^\",\"^g;s^\/\@^\"^g" > ${DEST}/${PREVIOUS_VERSIONS_DIR}/versions-list.json || echo "No previous versions"
+# }
 
-move_versions() {
-    mkdir -p ${DEST}
-    mkdir -p ${TMP_DEST}/${PREVIOUS_VERSIONS_DIR}
-    mkdir -p ${DEST}/${PREVIOUS_VERSIONS_DIR}
+# move_versions() {
+#     mkdir -p ${DEST}
+#     mkdir -p ${TMP_DEST}/${PREVIOUS_VERSIONS_DIR}
+#     mkdir -p ${DEST}/${PREVIOUS_VERSIONS_DIR}
 
-    if version_greater_equal ${LATEST_VERSION} ${PREVIOUS_VERSION}
-    then
-        if ["$CUT_PREVIOUS_VERSION" != "$CUT_LATEST_VERSION"]
-        then
-            echo "Move current version to previous versions."
-    else 
-        if ["$CUT_PREVIOUS_VERSION" != "$CUT_LATEST_VERSION"]
-        then
-            echo "Move deployment version to previous versions."
-    fi
+#     if version_greater_equal ${LATEST_VERSION} ${PREVIOUS_VERSION}
+#     then
+#         if ["$CUT_PREVIOUS_VERSION" != "$CUT_LATEST_VERSION"]
+#         then
+#             echo "Move current version to previous versions."
+#     else 
+#         if ["$CUT_PREVIOUS_VERSION" != "$CUT_LATEST_VERSION"]
+#         then
+#             echo "Move deployment version to previous versions."
+#     fi
 
-    echo "$LATEST_VERSION $PREVIOUS_VERSION"
-    rm -rf ${DEST}
-    mv ${TMP_DEST} ${DEST}
-    save_versions_list
-    echo $LATEST_VERSION > ${DEST}/last_deployed
-    chown -R ${FUNCTIONAL_USER}:${FUNCTIONAL_USER} ${DEST}
-    echo "--- Done ---"
-}
+#     echo "$LATEST_VERSION $PREVIOUS_VERSION"
+#     rm -rf ${DEST}
+#     mv ${TMP_DEST} ${DEST}
+#     save_versions_list
+#     echo $LATEST_VERSION > ${DEST}/last_deployed
+#     chown -R ${FUNCTIONAL_USER}:${FUNCTIONAL_USER} ${DEST}
+#     echo "--- Done ---"
+# }
