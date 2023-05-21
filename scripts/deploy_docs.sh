9f5f5fc6cef9191a9a18b35cc9768e5da8d07323
Joshua Oguma <joshua.oguma@outlook.com>
2023-05-21 15:40:04 +0300

    added some changes

--- scripts/deploy_docs.sh
+++ scripts/deploy_docs.sh
@@ -9,9 +9,9 @@ export CUT_PREVIOUS_VERSION=$(echo ${PREVIOUS_VERSION} | cut -d. -f1-2)
 export CUT_LATEST_VERSION=$(echo ${LATEST_VERSION} | cut -d. -f1-2)
 export PREVIOUS_VERSIONS_DIR=previous_versions
 
-# version_greater_equal() {
-#     printf '%s\n%s\n' "$2" "$1" | sort --check=quiet --version-sort
-# }
+version_greater_equal() {
+    printf '%s\n%s\n' "$2" "$1" | sort --check=quiet --version-sort
+}
 
 # save_versions_list() {
 #     ls ${DEST}/${PREVIOUS_VERSIONS_DIR} --hide versions-list.json | awk ' BEGIN {ORS = ""; print "[";}' | sed "s^\^\\\\\"^g;s^\/\@\/\@^\",\"^g;s^\/\@^\"^g" > ${DEST}/${PREVIOUS_VERSIONS_DIR}/versions-list.json || echo "No previous versions"
